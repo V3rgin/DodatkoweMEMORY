9a731dccbf0231622ce4e2cea4f8d17de5f60565
Vergin <mrtemnmie@gmail.com>
2024-10-24 16:00:58 +0200

    gaming

--- app/src/main/java/com/example/dodatkowememo/MainActivity.java
+++ app/src/main/java/com/example/dodatkowememo/MainActivity.java
@@ -8,17 +8,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+    }
+    public int[] throwDraw(){
+        Random r = new Random();
+        int[] values = new int[8];
+
+        for (int i = 0; i < values.length; i++){
+            values[i] = r.nextInt(8)+1;
+        }
+        return values;
     }
 }
